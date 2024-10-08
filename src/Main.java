cdb0ba8819019d24788b02c77b07b6734ca9cec6
miguelanguai <miguelangelguaivi@gmail.com>
2024-10-08 10:08:39 +0200

    add exercise 2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import formacion.ejercicio1.Gato;
+import formacion.ejercicio2.Vehiculo;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,5 +7,8 @@ public class Main {
     public static void main(String[] args) {
         Gato gato = new Gato();
         gato.hacerSonido();
+
+        Vehiculo vehiculo = new Vehiculo();
+        vehiculo.arrancar();
     }
 }
---
+++ src/formacion/ejercicio2/Vehiculo.java
@@ -0,0 +1,7 @@
+package formacion.ejercicio2;
+
+public final class Vehiculo {
+    public void arrancar(){
+        System.out.println("Brum");
+    }
+}
