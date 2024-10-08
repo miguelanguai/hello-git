466e17c2068b6007163393d8db29c36b653a7907
miguelanguai <miguelangelguaivi@gmail.com>
2024-10-08 11:01:17 +0200

    add exercise 7

---
+++ src/formacion/ejercicio7/RepositorioDePersonas.java
@@ -0,0 +1,32 @@
+package formacion.ejercicio7;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class RepositorioDePersonas {
+    private List<Persona> repo = new ArrayList<Persona>();
+
+    public List<Persona> getRepo() {
+        return repo;
+    }
+
+    public void setRepo(List<Persona> repo) {
+        this.repo = repo;
+    }
+
+    public void agregarPersona(Persona persona) {
+        repo.add(persona);
+    }
+
+    public void retirarPersona(Long id) {
+        this.repo = repo.stream().filter(persona -> !Objects.equals(persona.getId(), id)).collect(Collectors.toList());
+    }
+
+    public void listarPersonas() {
+        for (Persona persona : repo) {
+            System.out.println(persona.getName());
+        }
+    }
+}
